0f94c8786f39db5179031a794305fa24228ed2d8
hmz273 <73111873+hmz273@users.noreply.github.com>
2024-05-02 12:31:53 +0100

    change items base on language

--- src/app/compoments/About.tsx
+++ src/app/compoments/About.tsx
@@ -37,34 +37,48 @@ const About = () => {
       </select>
 
       <div className="grid max-w-screen-xl md:w-[70rem] py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center md:w-[524px] md:h-[445px] md:gap-[50px]  lg:col-span-7">
-          <h3 className=" md:text-lef font-normal md:text-[34px] md:leading-[37.4px] text-[42px] leading-[46.2px] tracking-normal text-gray-800">
-            {aboutDetails.title}
-          </h3>
+      {language === 'ar' ? (
+  <>
+    <div className="md:h-[564px] pl-[12px] md:w-[413px] lg:mt-0 lg:col-span-5 lg:flex">
+      <Image src={img} className="md:flex hidden md:w-[413px]" alt="mockup" />
+    </div>
+    <div className="mr-auto place-self-center md:w-[524px] md:h-[445px] md:gap-[50px] lg:col-span-7">
+      <h3 className="md:text-lef font-normal md:text-[34px] md:leading-[37.4px] text-[42px] leading-[46.2px] tracking-normal text-gray-800">
+        {aboutDetails.title} <span className="text-red-600 font-bold uppercase">Jardin</span>
+      </h3>
+      <h3 className="md:text-left uppercase py-12 font-quicksand font-normal text-[24px] leading-[26.4px] tracking-normal text-gray-800">
+        {etabTypeLabel} {aboutDetails.souTitle}
+      </h3>
+      <p className="md:text-left mb-6 font-quicksand font-normal line-clamp-[7] text-[18px] leading-[28.8px] text-gray-500 lg:mb-8 dark:text-gray-400">
+        {aboutDetails.description}
+      </p>
+      <a href="#" data-modal-target="about-modal" data-modal-toggle="about-modal" className="hidden text-left tracking-tight uppercase md:inline-flex mr-3 font-quicksand font-normal text-[18px] leading-[19.8px] text-[#AD936D]">
+        En savoir plus
+      </a>
+    </div>
+  </>
+) : (
+  <>
+    <div className="mr-auto place-self-center md:w-[524px] md:h-[445px] md:gap-[50px] lg:col-span-7">
+      <h3 className="md:text-lef font-normal md:text-[34px] md:leading-[37.4px] text-[42px] leading-[46.2px] tracking-normal text-gray-800">
+        {aboutDetails.title} <span className="text-red-600 font-bold uppercase">Jardin</span>
+      </h3>
+      <h3 className="md:text-left uppercase py-12 font-quicksand font-normal text-[24px] leading-[26.4px] tracking-normal text-gray-800">
+        {etabTypeLabel} {aboutDetails.souTitle}
+      </h3>
+      <p className="md:text-left mb-6 font-quicksand font-normal line-clamp-[7] text-[18px] leading-[28.8px] text-gray-500 lg:mb-8 dark:text-gray-400">
+        {aboutDetails.description}
+      </p>
+      <a href="#" data-modal-target="about-modal" data-modal-toggle="about-modal" className="hidden text-left tracking-tight uppercase md:inline-flex mr-3 font-quicksand font-normal text-[18px] leading-[19.8px] text-[#AD936D]">
+        En savoir plus
+      </a>
+    </div>
+    <div className="md:h-[564px] pl-[12px] md:w-[413px] lg:mt-0 lg:col-span-5 lg:flex">
+      <Image src={img} className="md:flex hidden md:w-[413px]" alt="mockup" />
+    </div>
+  </>
+)}
 
-          <h3
-            className="md:text-left uppercase py-12 font-quicksand font-normal text-[24px] leading-[26.4px] tracking-normal text-gray-800"
-
-          >
-            {etabTypeLabel} {aboutDetails.souTitle}
-          </h3>
-          <p
-
-            className=" md:text-left mb-6 font-quicksand font-normal line-clamp-[7] text-[18px] leading-[28.8px] text-gray-500 lg:mb-8 dark:text-gray-400"
-          >
-            {aboutDetails.description}
-          </p>
-          <a
-            href="#"
-            data-modal-target="about-modal" data-modal-toggle="about-modal"
-            className="hidden text-left tracking-tight uppercase md:inline-flex mr-3 font-quicksand font-normal text-[18px] leading-[19.8px] text-[#AD936D]"
-          >
-            En savoir plus
-          </a>
-        </div>
-        <div className="md:h-[564px] pl-[12px] md:w-[413px] lg:mt-0 lg:col-span-5 lg:flex">
-          <Image src={img} className="md:flex hidden md:w-[413px]" alt="mockup" />
-        </div>
       </div>
     </section>
     <AboutModel about={about.description} souTitle={about.souTitle} title={about.title}/>
